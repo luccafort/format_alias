796f81989856f710e1691f7006e8ef09b3a3d74a
luccafort <luccafort@gmail.com>
2016-04-22 02:27:54 +0900

    [add]time.Format()のエイリアス関数を新規作成
    time.Format()の日付フォーマットがわかりにくいためエイリアス関数を新規作成
    曜日を返す関数を作成する旨のコメントを追加

---
+++ format_alias.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Datetie 対応表に格納する構造体
+type Datetime struct {
+	src  string // time.Format()側のフォーマット
+	dest string // PHPなどで使用されている%[A-Za-z]なフォーマット
+}
+
+const DatetimeFormat = "2006-01-02 15:04:05"
+
+const DateFormat = "2006-01-02"
+
+const DefaultFormat = "2006-1-2 3:4:5"
+
+func main() {
+
+	now := time.Now()
+	now = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), 0, time.Local)
+
+	fmt.Printf("%q\n", now.Format(DatetimeFormat))
+	fmt.Printf("%q\n", now.Format(DateFormat))
+	fmt.Printf("%q\n", Format(now, "%Y-%m-%d %H:%i:%s"))
+	fmt.Printf("%q\n", Format(now, "%Y-%m-%d"))
+}
+
+// Time.Format()のAlias関数
+func Format(datetime time.Time, format string) string {
+	// TODO:本来はformatを""%[a-zA-z]"な形で正規表現でパース
+	// TODO:対応表(constで定義)を用意しておき対応した形でFormatなされていればOK
+	if format == "%Y-%m-%d %H:%i:%s" {
+		return datetime.Format(DatetimeFormat)
+	}
+	return datetime.Format(DefaultFormat)
+}
+
+// TODO:曜日を返す関数(日本語/英語両対応)
